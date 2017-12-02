3e5095528be0e873138b542991b1026bc9480933
Tejas Pancholi <tejas.er@outlook.com>
2017-12-02 11:25:34 +0530

    use actual expense data and color them based on amount value.

--- src/App.js
+++ src/App.js
@@ -1,89 +1,96 @@
-import React, { Component } from "react";
-import * as d3 from "d3";
-import "./App.css";
-
-var expenses = [
-  {
-    name: "Coffee",
-    amount: 5,
-    date: new Date()
-  },
-  {
-    name: "breakfast",
-    amount: 30,
-    date: new Date()
-  },
-  {
-    name: "fuel",
-    amount: 1000,
-    date: new Date()
-  },
-  {
-    name: "transport",
-    amount: 80,
-    date: new Date()
-  }
-];
+import React, { Component } from 'react';
+import * as d3 from 'd3';
+import chroma from 'chroma-js';
+import _ from 'lodash';
+import './App.css';
+import expenseData from './data/expenses.json';
+// console.log(expenseData);
 
 var width = 900;
 var height = 900;
-var radius = 10;
+var radius = 5;
+
+// d3 functions
+var colorScale = chroma.scale(['#53cf8d', '#f7d283', '#e85151']);
+var amountScale = d3.scaleLog();
 
 var simulation = d3
-  .forceSimulation()
-  .force("center", d3.forceCenter(width / 2, height / 2))
-  // .force("charge", d3.forceManyBody())
-  .force("collide", d3.forceCollide(radius))
-  .stop();
+    .forceSimulation()
+    .force('center', d3.forceCenter(width / 2, height / 2))
+    .force('charge', d3.forceManyBody())
+    .force('collide', d3.forceCollide(radius))
+    .stop();
 
 class App extends Component {
-  constructor(props) {
-    super();
-    this.forceTick = this.forceTick.bind(this);
-  }
-  componentWillMount() {
-    simulation.on("tick", this.forceTick);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            expenses: []
+        };
+        this.forceTick = this.forceTick.bind(this);
+    }
+    componentWillMount() {
+        var expenses = _.chain(expenseData)
+            .filter(d => d.Amount < 0)
+            .map(d => {
+                return {
+                    amount: -d.Amount,
+                    name: d.Description,
+                    date: d['Trans Date']
+                };
+            })
+            .value();
+        //process data
+        var amountExtent = d3.extent(expenses, d => d.amount);
+        amountScale.domain(amountExtent);
+        this.setState({ expenses });
 
-  componentDidMount() {
-    this.container = d3.select(this.refs.container);
-    // height and width can be provided here as well, however if they are not changed much give directly on the
-    // component itself.
-    // console.log(this.refs.container, this.container);  // just to check what those selection are providing us
-    this.renderCircles();
-    simulation
-      .nodes(expenses)
-      .alpha(0.9)
-      .restart();
-  }
+        //
+        simulation.on('tick', this.forceTick);
+    }
 
-  renderCircles() {
-    // now drawing circles
-    this.circles = this.container.selectAll("circle").data(expenses, d => d.name);
+    componentDidMount() {
+        this.container = d3.select(this.refs.container);
+        // height and width can be provided here as well, however if they are not changed much give directly on the
+        // component itself.
+        // console.log(this.refs.container, this.container);  // just to check what those selection are providing us
+        this.renderCircles();
+        simulation
+            .nodes(this.state.expenses)
+            .alpha(0.9)
+            .restart();
+    }
 
-    // complete enter, update and exit pattern.
+    renderCircles() {
+        // now drawing circles
+        this.circles = this.container.selectAll('circle').data(this.state.expenses, d => d.name);
 
-    // first we would need to carry out exit
-    this.circles.exit().remove();
+        // complete enter, update and exit pattern.
 
-    // after exit it will be enter + update
-    this.circles = this.circles
-      .enter()
-      .append("circle")
-      .merge(this.circles)
-      .attr("r", radius)
-      .attr("opacity", 0.5);
-  }
-  forceTick() {
-    console.log(this.circles.datum().x, this.circles.datum().y);
-    this.circles.attr("cx", d => d.x).attr("cy", d => d.y);
-  }
-  componentDidUpdate() {
-    this.renderCircles();
-  }
-  render() {
-    return <svg ref="container" width={width} height={height} />;
-  }
+        // first we would need to carry out exit
+        this.circles.exit().remove();
+
+        // after exit it will be enter + update
+        this.circles = this.circles
+            .enter()
+            .append('circle')
+            .attr('r', radius)
+            .attr('fill-opacity', 0.25)
+            .attr('stroke-width', 3)
+            .merge(this.circles)
+            .attr('fill', d => colorScale(amountScale(d.amount)))
+            .attr('stroke', d => colorScale(amountScale(d.amount)));
+    }
+    forceTick() {
+        // console.log(this.circles.datum().x, this.circles.datum().y);
+        this.circles.attr('cx', d => d.x).attr('cy', d => d.y);
+    }
+    componentDidUpdate() {
+        this.renderCircles();
+    }
+    render() {
+        return <svg ref="container" width={width} height={height} />;
+    }
 }
 
 export default App;
