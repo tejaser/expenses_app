321ab0310440330085f5256221d1bf9e33054d4d
Tejas Pancholi <tejas.er@outlook.com>
2017-12-02 15:19:01 +0530

    Changed data structure and created a basic visualization.

--- src/App.js
+++ src/App.js
@@ -8,17 +8,24 @@ import expenseData from './data/expenses.json';
 
 var width = 900;
 var height = 900;
-var radius = 5;
+var margin = { left: 20, top: 20, right: 20, bottom: 20 };
+var radius = 7;
 
 // d3 functions
+var xScale = d3
+    .scaleBand()
+    .domain([0, 1, 2, 3, 4, 5, 6])
+    .range([margin.left, width - margin.right]);
 var colorScale = chroma.scale(['#53cf8d', '#f7d283', '#e85151']);
 var amountScale = d3.scaleLog();
 
 var simulation = d3
     .forceSimulation()
     .force('center', d3.forceCenter(width / 2, height / 2))
-    .force('charge', d3.forceManyBody())
+    // .force('charge', d3.forceManyBody())
     .force('collide', d3.forceCollide(radius))
+    .force('x', d3.forceX(d => d.focusX))
+    .force('y', d3.forceY(d => d.focusY))
     .stop();
 
 class App extends Component {
@@ -36,13 +43,32 @@ class App extends Component {
                 return {
                     amount: -d.Amount,
                     name: d.Description,
-                    date: d['Trans Date']
+                    date: new Date(d['Trans Date'])
                 };
             })
             .value();
+
+        var row = -1;
+        expenses = _.chain(expenses)
+            .groupBy(d => d3.timeWeek.floor(d.date))
+            .sortBy((expenses, week) => new Date(week))
+            .map(expenses => {
+                row += 1;
+                return _.map(expenses, exp => {
+                    return Object.assign(exp, {
+                        focusX: xScale(exp.date.getDay()),
+                        focusY: row * 120
+                    });
+                });
+            })
+            .flatten()
+            .value();
+        // console.log(expenses);
+
         //process data
         var amountExtent = d3.extent(expenses, d => d.amount);
         amountScale.domain(amountExtent);
+
         this.setState({ expenses });
 
         //
